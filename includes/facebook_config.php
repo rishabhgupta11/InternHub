0b813793f88abac4a11f4e4d042aa71152206ef9
Rishabh Gupta <58866004+rishabhgupta11@users.noreply.github.com>
2020-12-21 21:45:43 +0530

    Update facebook_config.php

--- includes/facebook_config.php
+++ includes/facebook_config.php
@@ -10,8 +10,8 @@ if (!session_id())
 // Call Facebook API
 
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '368743007539171',
-  'app_secret'     => '1eb285d78e78fad7be989bc04845dbf4',
+  'app_id'      => ' ',
+  'app_secret'     => ' ',
   'default_graph_version'  => 'v2.10'
 ]);
 
